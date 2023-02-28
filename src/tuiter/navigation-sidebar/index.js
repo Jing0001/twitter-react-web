f0a01f525d4732285edcb64f7a1caee837628dbc
Jing0001 <feng.jing1@northeastern.edu>
2023-02-28 11:48:51 -0800

    finish exercise

---
+++ src/tuiter/navigation-sidebar/index.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {useLocation} from "react-router";
+
+
+const NavigationSidebar = (
+    {
+        active = 'explore'
+    }
+) => {
+    const {pathname} = useLocation();
+    const paths = pathname.split('/')
+
+    return (
+        <div className="list-group">
+            <a className="list-group-item d-inline-flex justify-content-start">
+                <span className="d-none d-xl-block">Tuiter</span>
+            </a>
+            <Link to="/tuiter/home"
+                  className={`list-group-item wd-noLinkLineDecor ${active === 'home' ? 'active' : ''}`}>
+                <span className="d-none d-xl-inline">Home</span>
+            </Link>
+
+            <Link to="/tuiter/explore"
+                  className={`list-group-item wd-noLinkLineDecor ${active === 'explore' ? 'active' : ''}`}>
+                <span className="d-none d-xl-inline">Explore</span>
+            </Link>
+
+            <Link to="/tuiter/notifications"
+                  className={`list-group-item wd-noLinkLineDecor ${active === 'notifications' ? 'active' : ''}`}>
+                <span className="d-none d-xl-inline">Notifications</span>
+            </Link>
+
+            <Link to="/tuiter/messages"
+                  className={`list-group-item wd-noLinkLineDecor ${active === 'messages' ? 'active' : ''}`}>
+                <span className="d-none d-xl-inline">Messages</span>
+            </Link>
+
+            <Link to="/tuiter/bookmarks"
+                  className={`list-group-item wd-noLinkLineDecor ${active === 'bookmarks' ? 'active' : ''}`}>
+                <span className="d-none d-xl-inline">Bookmarks</span>
+            </Link>
+
+            <Link to="/tuiter/lists"
+                  className={`list-group-item wd-noLinkLineDecor ${active === 'lists' ? 'active' : ''}`}>
+                <span className="d-none d-xl-inline">Lists</span>
+            </Link>
+
+            <Link to="/tuiter/profile"
+                  className={`list-group-item wd-noLinkLineDecor ${active === 'profile' ? 'active' : ''}`}>
+                <span className="d-none d-xl-inline">Profile</span>
+            </Link>
+
+            <Link to="/tuiter/more"
+                  className={`list-group-item wd-noLinkLineDecor ${active === 'more' ? 'active' : ''}`}>
+                        <span className="fa-stack wd-width20 fa-xs" >
+                        </span>
+                <span className="d-none d-xl-inline">More</span>
+            </Link>
+        </div>
+    );
+};
+export default NavigationSidebar;
+
+
