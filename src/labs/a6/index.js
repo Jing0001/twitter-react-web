1394d8555b213a57304b34747d4eca4c1d34a800
Jing0001 <feng.jing1@northeastern.edu>
2023-03-26 17:23:21 -0700

    add nav bar to all webpage

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -4,9 +4,11 @@ import ConditionalOutput
     from "./conditional-output";
 import TodoItem from "./todo/todo-item";
 import TodoList from "./todo/todo-list";
+import Nav from "../../nav";
 function Assignment6() {
     return (
         <div>
+            <Nav/>
             <h1>Assignment 6</h1>
             <Classes/>
             <Styles/>
--- src/labs/index.js
+++ src/labs/index.js
@@ -1,12 +1,9 @@
 import Assignment6 from "./a6";
-import Nav from "../../src/nav";
 import Assignment7 from "./a7";
 import {Routes, Route} from "react-router";
-import {Link} from "react-router-dom";
 function Labs() {
     return (
         <div>
-            <Nav/>
             <Routes>
                 <Route index
                        element={<Assignment6/>}/>
